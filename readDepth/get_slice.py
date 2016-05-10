076c40a7e8ed3d0b05689323ccfe75a5b597e603
Jeremy McRae <jm33@sanger.ac.uk>
2016-05-10 13:44:34 +0100

    added license headers and documented better. fixed import bug

--- readDepth/get_slice.py
+++ readDepth/get_slice.py
@@ -1,6 +1,27 @@
+'''
+Copyright (c) 2016 Genome Research Ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
 
 import os
 import subprocess
+import tempfile
 import shutil
 from getpass import getpass
 
@@ -30,7 +51,10 @@ def check_access(pwd_path=None):
     stdout = kinit.communicate(input=pwd)
 
 def get_bam_slice(bam_path, slice_bam, regions):
-    """ extract a section of a BAM file from IRODS, or lustre
+    """ extract a section of a BAM file from IRODS, or lustre.
+    
+    It can be much quicker extract a small slice of a BAM from IRODS, or to
+    iterate through a small slice of a BAM.
     
     Args:
         bam_path: path to BAM on IRODS, or lustre
