45db33b868d25337dc646a80fa1da7653bd873ae
Jeremy McRae <jm33@sanger.ac.uk>
2016-05-11 14:39:27 +0100

    changed string quotation marks

--- readDepth/constants.py
+++ readDepth/constants.py
@@ -20,4 +20,4 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
 # set the path to the version of samtools that can extract BAM slices from IRODS
-SAMTOOLS = "/software/hgi/pkglocal/samtools-1.3.1/bin/samtools"
+SAMTOOLS = '/software/hgi/pkglocal/samtools-1.3.1/bin/samtools'
