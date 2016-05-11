25f81b1e421b138860eed86c302e88be43428b04
Jeremy McRae <jm33@sanger.ac.uk>
2016-05-11 14:40:36 +0100

    added option for filtering by mapping quality. changed quotation marks.

--- readDepth/get_depth.py
+++ readDepth/get_depth.py
@@ -25,9 +25,11 @@ import os
 import tempfile
 
 from readDepth.get_slice import get_bam_slice, check_access
+from readDepth.allele_counter import AlleleCounter
 
-def get_read_depths(bam_path, variants, min_quality=0, by_strand=True, keep_bams=False):
-    """ get the read depths of the alleles at variable sites
+def get_read_depths(bam_path, variants, min_quality=0, map_quality=50,
+        by_strand=True, keep_bams=False):
+    ''' get the read depths of the alleles at variable sites
     
     Args:
         bam_path: path to BAM file (can be on a standard filesystem (e.g.
@@ -37,29 +39,30 @@ def get_read_depths(bam_path, variants, min_quality=0, by_strand=True, keep_bams
     
     Returns:
         list of dictionaries of allele depths by base.
-    """
+    '''
     
     pwd_path = os.path.expanduser('~/.kinit')
     check_access(pwd_path)
     
-    temp_bam = tempfile.NamedTemporaryFile(mode="wb", delete=not(keep_bams))
+    temp_bam = tempfile.NamedTemporaryFile(mode='wb', delete=not(keep_bams))
     get_bam_slice(bam_path, temp_bam, variants)
     
     counts = {}
-    counter = AlleleCounter(temp_bam.name, min_qual=min_quality, by_strand=by_strand)
+    counter = AlleleCounter(temp_bam.name, min_qual=min_quality,
+        map_qual=map_quality, by_strand=by_strand)
     for variant in variants:
         try:
             counts[variant] = counter.check_variant(*variant)
         except AssertionError:
-            counts[variant] = {"ref": None, "alt": None}
+            counts[variant] = {'ref': None, 'alt': None}
             if by_strand:
-                counts[variant] = {"ref": {"forward": None, "reverse": None},
-                    "alt": {"forward": None, "reverse": None}}
+                counts[variant] = {'ref': {'forward': None, 'reverse': None},
+                    'alt': {'forward': None, 'reverse': None}}
     
     if not keep_bams:
         # clean up the bam index (the BAM file will be automatically removed as it
         # is a tmpfile).
-        if os.path.exists("{}.bai".format(temp_bam.name)):
-            os.remove("{}.bai".format(temp_bam.name))
+        if os.path.exists('{}.bai'.format(temp_bam.name)):
+            os.remove('{}.bai'.format(temp_bam.name))
     
     return counts, temp_bam.name
