49c2f778c95eb6dfa33f759065ed96a9577d7446
Jeremy McRae <jm33@sanger.ac.uk>
2016-05-11 14:38:51 +0100

    added option for filtering by mapping quality. changed quotation marks

--- readDepth/allele_counter.py
+++ readDepth/allele_counter.py
@@ -25,8 +25,8 @@ import pysam
 
 class AlleleCounter(object):
     
-    def __init__(self, bam_path, max_coverage=1e10, min_qual=0, by_strand=False):
-        """ counts reads with different base calls at a chrom position
+    def __init__(self, bam_path, max_coverage=1e10, min_qual=0, map_qual=0, by_strand=False):
+        ''' counts reads with different base calls at a chrom position
         
         Args:
             bam_path: path to indexed bam file
@@ -34,38 +34,48 @@ class AlleleCounter(object):
             min_qual: minimum quality score required to include the base call
             by_strand: whether to separate the ref and alt counts by forward and
                 reverse reads.
-        """
+        '''
         
         self.bam = pysam.AlignmentFile(bam_path)
         self.max_coverage = max_coverage
         self.min_qual = min_qual
+        self.min_map_qual = map_qual
         self.by_strand = by_strand
     
     def check_variant(self, chrom, start_pos, end_pos=None, ref=None, alt=None):
-        """ counts reads with different base calls at a chrom position
+        ''' counts reads with different base calls at a chrom position
         
         Args:
-            chrom: chromosome to use eg "chr1" or "1" depending on how the BAM is
+            chrom: chromosome to use eg 'chr1' or '1' depending on how the BAM is
                 set up (specifically, an ID found in the BAMs sequence dictionary).
             start_pos: start_base position to count bases at.
             end_pos: end base position to count bases at, or None if single base SNV.
-            ref: reference allele (e.g. "T").
-            alt: alternate allele (e.g. "G").
+            ref: reference allele (e.g. 'T').
+            alt: alternate allele (e.g. 'G').
         
         Returns:
             dictionary of read counts indexed by base calls
-        """
+        '''
         
         # Make sure that if we have specified a ref allele, then we also have an alt
         # allele (and vice versa if we don't have the ref allele).
         assert type(ref) == type(alt)
         
+        start_pos = int(start_pos)
+        
+        if end_pos is None:
+            end_pos = start_pos + len(ref) - 1
+        
+        end_pos = int(end_pos)
         length = abs(end_pos - start_pos) + 1
-        alleles = {"ref": 0, "alt": 0}
+        
+        alleles = {'ref': 0, 'alt': 0}
         if self.by_strand:
-            alleles = {"ref": {"forward": 0, "reverse": 0}, "alt": {"forward": 0, "reverse": 0}}
+            alleles = {'ref': {'forward': 0, 'reverse': 0},
+                'alt': {'forward': 0, 'reverse': 0}}
         
         # count each base at the required site
+        # for pileupcolumn in self.bam.pileup(chrom, start_pos - 1, end_pos + 1, truncate=True, stepper='nofilter'):
         for pileupcolumn in self.bam.pileup(chrom, start_pos - 1, end_pos + 1, truncate=True):
             if pileupcolumn.pos != start_pos - 1:
                 continue
@@ -77,7 +87,7 @@ class AlleleCounter(object):
                 # reads with other alleles
                 if allele is not None:
                     if self.by_strand:
-                        strands = {False: "forward", True: "reverse"}
+                        strands = {False: 'forward', True: 'reverse'}
                         strand = strands[read.alignment.is_reverse]
                         alleles[allele][strand] += 1
                     else:
@@ -86,26 +96,31 @@ class AlleleCounter(object):
         return alleles
     
     def check_read(self, read, length, ref, alt):
-        """ gets the allele code for a variant site in a read
+        ''' gets the allele code for a variant site in a read
         
         Args:
             read: AlignmentRead from pileup
             length: length of variant in bp, from start pos to end_pos
             ref: sequence for reference allele
             alt: sequence for alternate alllele
-        """
+        '''
         
-        # convert the quality score to integer
-        qual = read.alignment.query_qualities[read.query_position:read.query_position + length][0]
+        pos = read.query_position
+        qual = self.min_qual + 1
+        if pos is not None:
+            qual = read.alignment.query_qualities[pos:pos + length][0]
         
         # ignore low qual reads
         if qual < self.min_qual:
             return None
         
+        if read.alignment.mapping_quality < self.min_map_qual:
+            return None
+        
         return self.check_allele(read, ref, alt, length)
     
     def check_allele(self, read, ref, alt, length):
-        """ checks whether the sequence at a site is for a "ref" or "alt" allele
+        ''' checks whether the sequence at a site is for a 'ref' or 'alt' allele
         
         Args:
             read: AlignmentRead from pileup
@@ -114,9 +129,9 @@ class AlleleCounter(object):
             alt: sequence for alternate alllele
         
         Returns:
-            "ref" or "alt" depending on whether the allele is for the reference
+            'ref' or 'alt' depending on whether the allele is for the reference
             sequence, or alternate to the reference sequence.
-        """
+        '''
         
         indel_length = (len(alt) - len(ref))
         
@@ -127,15 +142,15 @@ class AlleleCounter(object):
         allele = None
         if len(ref) > 1 or len(alt) > 1:
             if read.indel == 0:
-                allele = "ref"
+                allele = 'ref'
             elif read.indel == indel_length:
-                allele = "alt"
+                allele = 'alt'
         else:
             # if the variant is for a SNV, get the base call as a string
             seq = read.alignment.query_sequence[read.query_position:read.query_position + length]
             if seq == ref:
-                allele = "ref"
+                allele = 'ref'
             elif seq == alt:
-                allele = "alt"
+                allele = 'alt'
         
         return allele
